efd27dcef303accb7e2b0ecf13ba2f1ba6a3b1aa
Gregory Jose Vieira Russo <gregory.russo@dft-sp-wkn711.dafiti.local>
2020-01-10 09:24:47 -0300

    feature: implementando GlobalStyles

--- api-map/src/common/theme.js
+++ api-map/src/common/theme.js
@@ -1,10 +1,12 @@
 const breakpoints = ['40em', '52em', '64em', '80em']
 const space = [0, 4, 8, 16, 32, 64, 128, 256, 512]
+const fontFamily = 'Roboto, Arial, Helvetica, sans-serif'
 
 export default {
   color: '#000000',
   breakpoints,
   space,
+  fontFamily,
   colors: {
     texts: {
       primary: '#00306e'
--- api-map/src/views/App.js
+++ api-map/src/views/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
-import styled from 'styled-components'
+import { GlobalStyles } from '../common/GlobalStyles'
 import ContainerHeader from '../components/modules/ContainerHeader'
 import ContainerMaps from '../components/modules/ContainerMaps'
 
@@ -9,9 +9,8 @@ import theme from '../common/theme'
 const App = () => {
   const [data, setData] = useState('')
   return (
-    <ThemeProvider
-      theme={theme}
-    >
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <React.Fragment>
           <ContainerHeader setData={setData} />
           {data !== '' && <ContainerMaps data={data} setData={setData} />}
