124887952feeb6401c4a2fbcca0dc9099335aa63
Gregory Jose Vieira Russo <gregory.russo@dft-sp-wkn711.dafiti.local>
2020-01-10 23:41:33 -0300

    fix: alterando configuração de pastas

---
+++ build/precache-manifest.f32459c2867c481b516f1bd3a0ed0d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897ab6d52eed3c05cf495c0e7bc1da05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2af681755d7c04fa473",
+    "url": "/static/js/2.f2bfc013.chunk.js"
+  },
+  {
+    "revision": "dc96b2fe57711157dbbe5749faf088ee",
+    "url": "/static/js/2.f2bfc013.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb98df230d386ceae39f",
+    "url": "/static/js/main.0c74188c.chunk.js"
+  },
+  {
+    "revision": "b8467a4f62a5f28e7abb",
+    "url": "/static/js/runtime-main.323efb40.js"
+  }
+]);
