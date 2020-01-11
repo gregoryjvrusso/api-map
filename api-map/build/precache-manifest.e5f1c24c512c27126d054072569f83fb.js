b21137db235c26914c4c7049724c4f061f1f2e88
Gregory Jose Vieira Russo <gregory.russo@dft-sp-wkn711.dafiti.local>
2020-01-10 23:23:59 -0300

    feat: processo de deploy

---
+++ api-map/build/precache-manifest.e5f1c24c512c27126d054072569f83fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57802f7b6fbdefecf1b0ae2fc5e46502",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eff818da93f8c19c01f0",
+    "url": "/static/js/2.f0fc1195.chunk.js"
+  },
+  {
+    "revision": "dc96b2fe57711157dbbe5749faf088ee",
+    "url": "/static/js/2.f0fc1195.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42255d2aa1fe7b98fd21",
+    "url": "/static/js/main.8d00242e.chunk.js"
+  },
+  {
+    "revision": "f740eed4287c9ae2e401",
+    "url": "/static/js/runtime-main.092afe1f.js"
+  }
+]);
